8f4af33d4ebe769b5208b8d09049f0bc83d503bc
stefanygabriely <105082677+stefanygabriely@users.noreply.github.com>
2024-12-02 11:29:18 -0300

    ok

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('formulario').addEventListener("submit", function (event){
+    event.preventDefault();
+
+    //Obter as notas inserias pelo usuário
+    let nota1 = parseFloat(document.get)
+}
