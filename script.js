cbcaa15156a341af2ef1dc3f7686341ca1dc3b1e
stefanygabriely <105082677+stefanygabriely@users.noreply.github.com>
2024-12-03 09:25:26 -0300

    ok

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ document.getElementById('formulario').addEventListener("submit", function (event
     let resultadoDiv= document.getElementById('resultado');
     if(media>=6.){
         resultadoDiv.innerHTML= `<p class="aprovado">Aprovado!Sua média
-        foi:${media.toFixed(2)}</p>
-    }
-
-
-    });
+        foi:${media.toFixed(2)}</p>`;
+     }else{
+        resultadoDiv.innerHTML= `<p class="recuperacao">.sua média foi:
+        ${média.nota(2)}</p>`; 
+     }
+});
