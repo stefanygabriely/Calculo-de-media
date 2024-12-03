a0f35f3628a6d735fb69a9e61c6b317be31be9b1
stefanygabriely <105082677+stefanygabriely@users.noreply.github.com>
2024-12-03 09:13:18 -0300

    ok

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ document.getElementById('formulario').addEventListener("submit", function (event
     event.preventDefault();
 
     //Obter as notas inserias pelo usuário
-    let nota1 = parseFloat(document.get)
-}
+    let nota1 = parseFloat(document.getElementById("NOTA 1").value);
+    let nota2= parseFloat(document.getElementById('nota2').value);
+    let nota3= parseFloat(document.getElementById('nota2').value);
+    // caucular a media
+    let media=( nota1 + nota2+nota3)/ 3;
+
+    //Exibir a mensagem de resultado
+    let resultadoDiv= document.getElementById('resultado');
+    if(media>=6.){
+        resultadoDiv.innerHTML= `<p class="aprovado">Aprovado!Sua média
+        foi:${media.toFixed(2)}</p>
+    }
+
+
+    });
